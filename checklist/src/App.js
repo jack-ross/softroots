effb740dbccefb7abe7eb6cd7294cc57f5d87644
Michael Crawford <mdcrawford96@gmail.com>
2017-07-13 15:47:31 -0400

    changed some comments

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import TopNavBar from "./components/TopNavBar.js";
-import DropdownSelection from "./components/DropdownSelection.js";
 
 import CreateOrEditChecklist from "./pages/CreateOrEditChecklist.js";
 import Home from "./pages/Home.js";
@@ -11,44 +9,10 @@ import ViewChecklists from "./pages/ViewChecklists.js";
 
 import "./App.css";
 
-const tabs = [
-  {
-    name: "Home",
-    url: "/"
-  },
-  {
-    name: "Create a Checklist",
-    url: "/createchecklist"
-  },
-  {
-    name: "View Current Checklists",
-    url: "/viewchecklists"
-  },
-  {
-    name: "Manage Users",
-    url: "/users"
-  }
-];
-
 class App extends Component {
   onClick(value) {
     console.log(value);
   }
-
-  /*
-  render() {
-    return (
-      <div className="App">
-        <TopNavBar className={""} tabs={""} currentURL="/" />
-        <DropdownSelection
-          defaultText="Choose a player"
-          dropdownValues={["link", "mario", "zelda"]}
-          onClickField={value => this.onClick(value)}
-        />
-      </div>
-    );
-  }
-  */
 
   render() {
     return (
