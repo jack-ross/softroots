b0a89417b8f1bec9553c0a28aa293179e34cb46a
Michael Crawford <mdcrawford96@gmail.com>
2017-07-13 15:51:51 -0400

    deleted some things that didn't matter

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
+// the different pages that can be rendered
 import CreateOrEditChecklist from "./pages/CreateOrEditChecklist.js";
 import Home from "./pages/Home.js";
 import UserManagement from "./pages/UserManagement.js";
 import ViewChecklists from "./pages/ViewChecklists.js";
-
 
 import "./App.css";
 
@@ -16,8 +16,7 @@ class App extends Component {
   }
 
   render() {
-    return (  
-      
+    return (
       <BrowserRouter>
         <div className="App">
           <Switch>
@@ -29,7 +28,7 @@ class App extends Component {
             />
             <Route exact path="/viewchecklists" component={ViewChecklists} />
             <Route exact path="/users" component={UserManagement} />
-          
+
             <Route
               render={function() {
                 return <p>Not Found</p>;
