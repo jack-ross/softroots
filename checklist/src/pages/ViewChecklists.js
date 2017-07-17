15d4b2338102e5177e3e3049d5bce51b7014afed
Michael Crawford <mdcrawford96@gmail.com>
2017-07-17 11:18:28 -0400

    fixed counting bug in dynamic list, added ability for multiple inputs

--- checklist/src/pages/ViewChecklists.js
+++ checklist/src/pages/ViewChecklists.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TopNavBar from "../components/TopNavBar.js";
+import CollapseableList from "../components/CollapseableList.js";
 
 const tabs = [
   {
