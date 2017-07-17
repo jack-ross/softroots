cabbcf62e8b8f90831064b467841a4fc2963ad2e
Michael Crawford <mdcrawford96@gmail.com>
2017-07-17 11:23:34 -0400

    Update ViewChecklists.js

--- checklist/src/pages/ViewChecklists.js
+++ checklist/src/pages/ViewChecklists.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import TopNavBar from "../components/TopNavBar.js";
-import CollapseableList from "../components/CollapseableList.js";
 
 const tabs = [
   {
