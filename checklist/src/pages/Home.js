cb4855f8056f6011317a785db6bde99039d616c6
Michael Crawford <mdcrawford96@gmail.com>
2017-07-18 10:18:44 -0400

    created UI for resorting tasks in checklist

--- checklist/src/pages/Home.js
+++ checklist/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import TopNavBar from "../components/TopNavBar.js";
 import Check from "../components/Check.js";
-import Sort from "../components/Sort.js";
+import SortableTasks from "../components/SortableTasks.js";
 
 const tabs = [
   {
@@ -22,18 +22,31 @@ const tabs = [
   }
 ];
 
+const dummyData = [
+  "Clean the bathroom",
+  "Prep food for tomorrow",
+  "Mop the back"
+];
+
 export default class Home extends Component {
+  forChild(a, b) {
+    console.log(a + " " + b);
+  }
+
   render() {
     return (
       <div>
         <TopNavBar className="horizontal" tabs={tabs} currentURL="/" />
         <p> Home </p>
         <div>
-        <Check/>
+          <Check />
         </div>
         <div>
-          <Sort/>
-          </div>
+          <SortableTasks
+            initialValues={dummyData}
+            updateParent={(a, b) => this.forChild(a, b)}
+          />
+        </div>
       </div>
     );
   }
