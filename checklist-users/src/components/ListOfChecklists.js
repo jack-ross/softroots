c6882f29656c0aa376862094418d50370c9f5acc
Michael Crawford <mdcrawford96@gmail.com>
2017-08-11 11:04:05 -0400

    the titles of checklists are now colored based on if the entire checklist has been completed

--- checklist-users/src/components/ListOfChecklists.js
+++ checklist-users/src/components/ListOfChecklists.js
@@ -3,6 +3,8 @@ import { Collapse } from "antd";
 import ViewSingleChecklist from "./ViewSingleChecklist.js";
 import createEndTimeString from "../helperFunctions/createEndTimeString.js";
 import sortEndTimes from "../helperFunctions/sortEndTimes.js";
+import isChecklistCompleted from "../helperFunctions/isChecklistCompleted.js";
+import isPastEndTime from "../helperFunctions/isPastEndTime.js";
 
 /* PROPS:
     checklists: [obj], the checklist objects pulled from firebase to render
@@ -12,19 +14,45 @@ import sortEndTimes from "../helperFunctions/sortEndTimes.js";
 
 export default class ListOfChecklists extends Component {
   render() {
+    // create an array of the checklists
     let checklistArray = Object.keys(this.props.checklists).map(key => {
       return this.props.checklists[key];
     });
 
+    // sort them based on their end times
     checklistArray.sort(function(a, b) {
       return sortEndTimes(a.endTime, b.endTime);
     });
+
+    // the CSS color constants
+    const red = "red";
+    const green = "green";
+    const darkOrange = "#bf833b";
+
+    // render each checklist as a Collapse from antd, where the inner panel
+    // displays the checklist itself as a ViewSingleChecklist component.
+    // the color of the text in the header is determined by whether or not
+    // the checklist is completed and if it's late
     const renderedChecklists = checklistArray.map(checklist => {
       let headerWithEndTime =
-        checklist.title +
-        " (due by " +
-        createEndTimeString(checklist.endTime) +
-        ")";
+        checklist.title + " (" + createEndTimeString(checklist.endTime) + ")";
+
+      // determine the header color
+      let headerStyle = { color: green };
+      if (!isChecklistCompleted(checklist)) {
+        if (isPastEndTime(checklist.endTime, "America/New_York")) {
+          headerStyle.color = red;
+        } else {
+          headerStyle.color = darkOrange;
+        }
+      }
+
+      headerWithEndTime = (
+        <p style={headerStyle}>
+          {" "}{headerWithEndTime}{" "}
+        </p>
+      );
+
       return (
         <Collapse.Panel header={headerWithEndTime}>
           <ViewSingleChecklist
@@ -34,6 +62,7 @@ export default class ListOfChecklists extends Component {
         </Collapse.Panel>
       );
     });
+
     return (
       <Collapse accordion>
         {renderedChecklists}
