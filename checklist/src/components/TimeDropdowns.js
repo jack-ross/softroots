65b456f3b86d646eaa7fd2d24ad5e244aab88644
Matt McGuiness <mattjmcguiness@gmail.com>
2017-11-09 23:21:27 -0500

    Remove unused import

--- checklist/src/components/TimeDropdowns.js
+++ checklist/src/components/TimeDropdowns.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, TimePicker } from "antd";
+import { Button } from "antd";
 import DropdownSelection from "./DropdownSelection.js";
 import "../css/ChecklistForm.css";
 
