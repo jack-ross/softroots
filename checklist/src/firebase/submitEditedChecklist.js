5055791f5ae3b9cffb4b80436dbd1e9b32b200d4
Matt McGuiness <mattjmcguiness@gmail.com>
2018-01-08 22:54:41 -0500

    Refactor ViewChecklsit

--- checklist/src/firebase/submitEditedChecklist.js
+++ checklist/src/firebase/submitEditedChecklist.js
@@ -8,86 +8,48 @@ import { notification } from "antd";
 import sortDaysOfWeek from "../helperFunctions/sortDaysOfWeek.js";
 
 export default function submitEditedChecklist(
-  checklist,
-  initialLocations,
-  initialRole
+  checklist
 ) {
-  // first, clean up locations to the proper strings (i.e. charlottesville_va)
-  // still hardcoded in for Roots
-  let locationKeys = checklist.locations;
+  console.log('this is the checklist', checklist);
+  let location = checklist.location;
 
-  // sort the checklist's days of the week
-  checklist.daysToRepeat.sort(sortDaysOfWeek);
-
-  // first, use helper function to create three disjoint arrays: original locations
-  // no longer relevant, overlap between original locations and new locations, and finally
-  // the new locations not originally present
-  let disjointLocationArrays = createDisjointArrays(
-    initialLocations,
-    locationKeys
-  );
-
-  let exclusiveInitialLocations = disjointLocationArrays.disjointFirstArray;
-  let exclusiveNewLocations = disjointLocationArrays.disjointSecondArray;
-  let overlapLocations = disjointLocationArrays.overlap;
-
-  let currentRole = checklist.role;
-
-  // this object will keep track of ALL the updates that will need to be performed
-  // using firebase's update() method; the keys are the firebase paths and the values
-  // are either the checklist (if data is being stored at that path) or null (if deleting
-  // data from that path)
   let firebaseUpdates = {};
+  // make a copy of the checklist object (because JavaScript is weird with how
+  // objects work as parameters)
+  let newChecklist = Object.assign({}, checklist);
 
-  // for exclusively old locations, go to that location and the initial role and
-  // delete checklist
-  exclusiveInitialLocations.map(location => {
-    let path =
-      "/checklists/" + location + "/" + initialRole + "/" + checklist.key;
-    firebaseUpdates[path] = null;
-  });
+  // generate the key for this checklist
+  let checklistKey = checklist.key;
+  newChecklist.key = checklistKey;
 
-  // for exclusively new locations, go to that location and the current role and add
-  // the checklist
-  exclusiveNewLocations.map(location => {
-    let path =
-      "/checklists/" + location + "/" + currentRole + "/" + checklist.key;
-    firebaseUpdates[path] = checklist;
-  });
+  // generate the path based on location and role
+  let path =
+    "/checklists/" + location + "/" + checklist.role + "/" + checklistKey;
 
-  // finally, for overlapping locations, compare the intitial role with current role;
-  // if both the same, simply update the checklist at that spot in firebase.  else, delete
-  // from original path and update in new path based on the change in role
-  overlapLocations.map(location => {
-    if (currentRole === initialRole) {
-      let path =
-        "/checklists/" + location + "/" + currentRole + "/" + checklist.key;
-      firebaseUpdates[path] = checklist;
-    } else {
-      let pathToDelete =
-        "/checklists/" + location + "/" + initialRole + "/" + checklist.key;
-      firebaseUpdates[pathToDelete] = null;
-      let pathToUpdate =
-        "/checklists/" + location + "/" + currentRole + "/" + checklist.key;
-      firebaseUpdates[pathToUpdate] = checklist;
-    }
-  });
+  // this version of the checklist will hold an array with just the ONE location
+  newChecklist.location = [location];
 
-  // make all the firebase calls
+  // sort the days to repeat
+  newChecklist.daysToRepeat.sort(sortDaysOfWeek);
+
+  // add to the firebase updates
+  firebaseUpdates[path] = newChecklist;
+
+  // make the firebase call
   firebase
     .database()
     .ref()
     .update(firebaseUpdates)
     .then(response => {
       notification.success({
-        message: "Changes Saved",
-        description: "Your changes were successfully saved."
+        message: "SUCCESS",
+        description: "Your checklist has been successfully saved."
       });
     })
     .catch(error => {
       notification.error({
         message: "ERROR",
-        description: "Firebase error: " + error
+        description: error.message
       });
     });
 }
